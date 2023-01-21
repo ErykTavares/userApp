8278b9677912d0ee9b26ca0c4b5d0b32fcad129e
ErykTavares <erictavares35@gmail.com>
2023-01-20 23:34:37 -0300

    fixed, imports relative path issue

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,23 @@
 module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
+	presets: ['module:metro-react-native-babel-preset'],
+	plugins: [
+		[
+			'module-resolver',
+			{
+				root: ['.'],
+				extensions: [
+					'.ios.js',
+					'.android.js',
+					'.js',
+					'.ts',
+					'.tsx',
+					'.json',
+				],
+				alias: {
+					'@components': './src/components',
+					'@': './src',
+				},
+			},
+		],
+	],
 };
